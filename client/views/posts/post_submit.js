0b053bf4ec64f5698d64ac8b363749095c74f397
rickydavila <richardadavila@gmail.com>
2013-11-10 16:01:35 -0600

    9-2

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -9,8 +9,15 @@ Template.postSubmit.events({
       message: $(e.target).find('[name=message]').val()
     };
     Meteor.call('post', post, function(error, id) {
-      if(error)
-        return alert(error.reason);
+      if(error) {
+        // display the error to the user
+        throwError(error.reason);
+        // if the error is that the post already exists, take us there
+        if (error.error === 302)
+          Meteor.Router.to('postPage', error.details);
+      } else {
+        Meteor.Router.to('postPage', id);
+      }
     });
     Meteor.Router.to('postsList');
     //setting id of post to the return db id value Posts.insert returns
