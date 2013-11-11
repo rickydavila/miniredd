93d31eee3a9636d62f1c6c6d7ede02cb9cbd37de
rickydavila <richardadavila@gmail.com>
2013-11-11 12:45:40 -0600

    12-4 for real

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-Meteor.subscribeWithPagination('newPosts', 10);
+postsHandle = Meteor.subscribeWithPagination('newPosts', 10);
 Deps.autorun(function() {
   Meteor.subscribe('singlePost', Session.get('currentPostId'));
   Meteor.subscribe('comments', Session.get('currentPostId'));
