5bdf8262f785a7e33f63c952c96ccc98d3a3ca5f
rickydavila <richardadavila@gmail.com>
2013-11-11 12:43:47 -0600

    12-4

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
-Meteor.subscribeWithPagination('posts', 10);
+Meteor.subscribeWithPagination('newPosts', 10);
 Deps.autorun(function() {
+  Meteor.subscribe('singlePost', Session.get('currentPostId'));
   Meteor.subscribe('comments', Session.get('currentPostId'));
 });
 Meteor.subscribe('notifications');
