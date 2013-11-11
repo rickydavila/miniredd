6bcff591ffb4489367a0453ae8af7769fcc70746
rickydavila <richardadavila@gmail.com>
2013-11-11 17:13:36 -0600

    13-1 for real

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -8,3 +8,9 @@ Template.postItem.helpers({
     return a.hostname;
   }
 });
+Template.postItem.events({
+  'click .upvote': function(e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
+  }
+});
